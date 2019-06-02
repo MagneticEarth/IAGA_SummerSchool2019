7490e1925d52aa9cfb544d140cd7c5ba9997ceb3
smithara <smithara@local>
2019-06-02 17:33:52 +0000

    First changes towards PEP8 compliance.

--- src/mag_lib.py
+++ src/mag_lib.py
@@ -5,11 +5,13 @@ Created on Mon Feb  5 12:27:07 2018
 @author: djk
 """
 
+import os
 import pandas as pd
 import numpy as np
 
 r2d = np.rad2deg
 d2r = np.deg2rad
+
 
 def IAGA2002_Header_Reader(IAGA2002_file):
     """
@@ -79,8 +81,7 @@ def IAGA2002_Header_Reader(IAGA2002_file):
     n_comment  = n_lines-n_header     # The number of comment lines
     n_header  -= 1                    # The number of header lines
     return (n_header, n_comment, headers)
-    
-###############################################################################  
+
 
 def IAGA2002_Data_Reader(IAGA2002_file):
     """
@@ -111,15 +112,15 @@ def IAGA2002_Data_Reader(IAGA2002_file):
     5 Feb 2018
       
     """
-# Read the header and comment lines at the top of the file to get the number
-# of rows to skip before reading the data
+    # Read the header and comment lines at the top of the file to get the number
+    # of rows to skip before reading the data
     header = IAGA2002_Header_Reader(IAGA2002_file)
     nskip  = header[0]+header[1]
 
-# Read the data into a pandas dataframe (an IAGA2002 file has 'DATE' and 'TIME'
-# as the first two column labels.) There's a trailing '|' on the column header
-# line which is interpreted as the header for a column of nans and this 
-# property is used to delete it. 
+    # Read the data into a pandas dataframe (an IAGA2002 file has 'DATE' and 'TIME'
+    # as the first two column labels.) There's a trailing '|' on the column header
+    # line which is interpreted as the header for a column of nans and this 
+    # property is used to delete it. 
 
     DT_INDEX = 'DATE_TIME'
     vals = pd.read_csv(IAGA2002_file, 
@@ -130,10 +131,39 @@ def IAGA2002_Data_Reader(IAGA2002_file):
     vals.dropna(inplace=True, axis=1)
     
     return(vals)
+
+
+def load_year(observatory=None, year=None, path=None):
+    """Read in the daily 1-min files from a whole year.
     
-###############################################################################  
-"""
-Created on Tue Jan 29 20:43:35 2019
+    Parameters
+    ----------
+        observatory: string
+            Observatory code e.g. ESK
+        year: int/string
+            Desired year to load
+        path: string
+            Directory containing the files for that year
+    
+    Returns
+    -------
+        DataFrame
+    
+    """
+    dates_in_year = pd.date_range(
+        start=f'{year}-01-01', end=f'{year}-12-31', freq='D'
+    )
+    df = pd.DataFrame()
+    for date in dates_in_year:
+        ymd = date.strftime('%Y%m%d')
+        file_name = f'{observatory}{ymd}dmin.min'
+        file_path = os.path.join(path, file_name)
+        df = df.append(IAGA2002_Data_Reader(file_path))
+    return df
+
+
+def read_obs_hmv(obscode, year_st, year_fn, folder):
+    """Read in observatory annual mean files in IAGA2002 format.
     
     This function reads the hourly mean value data in yearly IAGA2002 format
     files into a pandas dataframe. (Note: The data may be reported in different
@@ -164,15 +194,10 @@ Created on Tue Jan 29 20:43:35 2019
     -------------
     30 Jan 2019
     
-Function to read in observatory annual mean files in IAGA2002 format
-
-@author: djk
-"""
-
-def read_obs_hmv(obscode, year_st, year_fn, folder):
+    """
     OBSY   = obscode.upper()
     obsy   = obscode.lower()
-# Read in the observatory data one year file at a time and construct filenames
+    # Read in the observatory data one year file at a time and construct filenames
     datareq = pd.DataFrame()        
     for year in range(year_st, year_fn+1):
         ystr    = str(year)
@@ -187,11 +212,9 @@ def read_obs_hmv(obscode, year_st, year_fn, folder):
         datareq = datareq.append(tmp[['X','Y', 'Z']])
     return(datareq)
     
-###############################################################################  
-"""
-Created on Tue Jan 29 20:43:35 2019
-    
-    Function to read in the annual mean values for a single observatory
+
+def read_obs_ann_mean(obscode, filename):
+    """Read in the annual mean values for a single observatory.
       
     Input parameters
     ---------------
@@ -211,10 +234,7 @@ Created on Tue Jan 29 20:43:35 2019
     -------------
     30 Jan 2019
 
-@author: djk
-
-""" 
-def read_obs_ann_mean(obscode, filename):
+    """
     count = 0
     with open(filename) as ofile:
         for line in ofile:
@@ -226,12 +246,9 @@ def read_obs_ann_mean(obscode, filename):
                 count +=1
     return(df)
 
-###############################################################################  
-"""
-Created on Tue Jan 29 20:43:35 2019
     
-    Function to read in the annual mean values for all observatories in a
-    given year
+def obs_ann_means_one_year(year, filename):
+    """Read in the annual mean values for all observatories in a given year.
       
     Input parameters
     ---------------
@@ -251,10 +268,7 @@ Created on Tue Jan 29 20:43:35 2019
     -------------
     30 Jan 2019
 
-@author: djk
-
-"""     
-def obs_ann_means_one_year(year, filename):
+    """
     
     count = 0
     with open(filename) as ofile:
@@ -266,14 +280,11 @@ def obs_ann_means_one_year(year, filename):
                 df.loc[count] = line.split()
                 count +=1
     return(df)
-    
-###############################################################################
-"""
 
-Created on Tue Jan 29 20:43:35 2019
-    
-    Calculate X and Y from D (in 0.1 minutes) and H as reported in IAGA2002
-    format files
+   
+def dh2xy(d, h):
+    """Calculate X and Y from D (in 0.1 minutes) and H as reported in IAGA2002
+    format files.
       
     Input parameters
     ---------------
@@ -292,20 +303,13 @@ Created on Tue Jan 29 20:43:35 2019
     -------------
     30 Jan 2019
 
-@author: djk
-
-""" 
-   
-def dh2xy(d, h):
+    """
     dec = d*np.pi/(180.*60.)
     return((h*np.cos(dec), h*np.sin(dec)))
-    
-###############################################################################
-"""
 
-Created on Tue Jan 29 20:43:35 2019
-    
-    Calculate D, H, I and F from (X, Y, Z)
+
+def xyz2dhif(x, y, z):
+    """Calculate D, H, I and F from (X, Y, Z)
       
     Input parameters
     ---------------
@@ -328,11 +332,8 @@ Created on Tue Jan 29 20:43:35 2019
     Revision date
     -------------
     28 April 2019
-    
-@author: djk
 
-"""
-def xyz2dhif(x, y, z):
+    """
     hsq = x*x + y*y
     hoz  = np.sqrt(hsq)
     eff = np.sqrt(hsq + z*z)
@@ -340,12 +341,9 @@ def xyz2dhif(x, y, z):
     inc = np.arctan2(z,hoz)
     return((r2d(dec), hoz, r2d(inc), eff))
 
-###############################################################################
-"""
 
-Created on Tue Jan 29 20:43:35 2019
-    
-    Calculate secular variation in D, H, I and F from (X, Y, Z) and
+def xyz2dhif_sv(x, y, z, xdot, ydot, zdot):
+    """Calculate secular variation in D, H, I and F from (X, Y, Z) and
     (Xdot, Ydot, Zdot)
       
     Input parameters
@@ -369,11 +367,8 @@ Created on Tue Jan 29 20:43:35 2019
     Revision date
     -------------
     28 April 2019
-    
-@author: djk
 
-"""
-def xyz2dhif_sv(x, y, z, xdot, ydot, zdot):
+    """
     h2  = x*x + y*y
     h   = np.sqrt(h2)
     f2  = h2 + z*z
--- src/sha_lib.py
+++ src/sha_lib.py
@@ -4,31 +4,38 @@ Created on Wed Jan 16 14:46:31 2019
 
 @author: djk
 """
-import numpy as np
-# Geodetic to geocentric colatitude calculation
-# Use WGS84 values for equatorial radius and reciprocal flattening 
-def gd2gc(h, gdcolat):
-        eqrad = 6378.137
-        flat  = 1/298.257223563
-        plrad = eqrad*(1-flat)
-        ctgd  = np.cos(np.deg2rad(gdcolat))
-        stgd  = np.sin(np.deg2rad(gdcolat))
-        a2    = eqrad*eqrad
-        a4    = a2*a2
-        b2    = plrad*plrad
-        b4    = b2*b2
-        c2    = ctgd*ctgd
-        s2    = 1-c2
-        rho   = np.sqrt(a2*s2 + b2*c2)
-        rad   = np.sqrt(h*(h+2*rho) + (a4*s2+b4*c2)/rho**2)
-        cd    = (h+rho)/rad
-        sd    = (a2-b2)*ctgd*stgd/(rho*rad)
-        cthc  = ctgd*cd - stgd*sd           # Also: sthc = stgd*cd + ctgd*sd
-        thc   = np.rad2deg(np.arccos(cthc)) # arccos returns values in [0, pi]
-        return((rad, thc, sd, cd))
 
-# Calculate values of (a/r)^(n+2) for n=0, 1, 2 ..., nmax
+import numpy as np
+
+
+def gd2gc(h, gdcolat):
+    """Geodetic to geocentric colatitude calculation.
+
+    Use WGS84 values for equatorial radius and reciprocal flattening
+
+    """
+    eqrad = 6378.137
+    flat  = 1/298.257223563
+    plrad = eqrad*(1-flat)
+    ctgd  = np.cos(np.deg2rad(gdcolat))
+    stgd  = np.sin(np.deg2rad(gdcolat))
+    a2    = eqrad*eqrad
+    a4    = a2*a2
+    b2    = plrad*plrad
+    b4    = b2*b2
+    c2    = ctgd*ctgd
+    s2    = 1-c2
+    rho   = np.sqrt(a2*s2 + b2*c2)
+    rad   = np.sqrt(h*(h+2*rho) + (a4*s2+b4*c2)/rho**2)
+    cd    = (h+rho)/rad
+    sd    = (a2-b2)*ctgd*stgd/(rho*rad)
+    cthc  = ctgd*cd - stgd*sd           # Also: sthc = stgd*cd + ctgd*sd
+    thc   = np.rad2deg(np.arccos(cthc)) # arccos returns values in [0, pi]
+    return((rad, thc, sd, cd))
+
+
 def rad_powers(n, a, r):
+    """Calculate values of (a/r)^(n+2) for n=0, 1, 2 ..., nmax"""
     arp = np.zeros(n+1)
     t0  = a/r
     arp[0] = t0*t0
@@ -36,8 +43,9 @@ def rad_powers(n, a, r):
         arp[i+1] = t0*arp[i]
     return(arp)
 
-# Populate arrays with cos(m*phi), sin(m*phi)
+
 def csmphi(m,phi):
+    """Populate arrays with cos(m*phi), sin(m*phi)"""
     cmp = np.zeros(m+1)
     smp = np.zeros(m+1)
     cmp[0] = 1
@@ -49,8 +57,9 @@ def csmphi(m,phi):
         smp[i+1] = smp[i]*cp + cmp[i]*sp
     return((cmp,smp))
 
-# Populate arrays with terms such as g(3,2)*cos(2*phi)*(a/r)**5 
+
 def gh_phi_rad(gh, nmax, cp, sp, rp):
+    """Populate arrays with terms such as g(3,2)*cos(2*phi)*(a/r)**5 """
     rx = np.zeros(nmax*(nmax+3)//2+1)
     ry = np.zeros(nmax*(nmax+3)//2+1)
     igx=-1
@@ -66,8 +75,9 @@ def gh_phi_rad(gh, nmax, cp, sp, rp):
             ry[igx] = (gh[igh-1]*sp[j] - gh[igh]*cp[j])*rp[i]
     return((rx, ry))
 
-# As above but without the radial function
+
 def gh_phi(gh, nmax, cp, sp):
+    """As for gh_phi_rad but without the radial function"""
     rx = np.zeros(nmax*(nmax+3)//2+1)
     igx=-1
     igh=-1
@@ -81,24 +91,28 @@ def gh_phi(gh, nmax, cp, sp):
             rx[igx] = (gh[igh-1]*cp[j] + gh[igh]*sp[j])
     return(rx)
 
-# Index for terms of degree=n and order=m in arrays pnm, xnm, ynm and znm
+
 def pnmindex(n,m):
+    """Index for terms of degree=n and order=m in arrays pnm, xnm, ynm and znm"""
     return(n*(n+1)//2+m)
-    
+
+
 def gnmindex(n,m):
     if(m==0):
         igx = n*n
     else:
         igx = n*n+2*m-1
     return(igx)
-    
+
+
 def hnmindex(n,m):
     return(n*n+2*m)
-    
-# Calculate arrays of the Associated Legendre Polynomials pnm 
-def pnm_calc(nmax, th):
 
-# Initialise
+
+def pnm_calc(nmax, th):
+    """Calculate arrays of the Associated Legendre Polynomials pnm"""
+
+    # Initialise
     nel   = nmax*(nmax+3)//2+1
     pnm   = np.zeros(nel)
     ct    = np.cos(np.deg2rad(th)); st = np.sin(np.deg2rad(th))
@@ -106,7 +120,7 @@ def pnm_calc(nmax, th):
     if(nmax==0): return(pnm)
     pnm[1] = ct
     pnm[2] = st
-#
+
     for i in range(2,nmax+1): # Loop over degree
         idx0 = pnmindex(i,i)
         idx1 = pnmindex(i-1,i-1)
@@ -122,14 +136,14 @@ def pnm_calc(nmax, th):
             pnm[idx0] = (t1*ct*pnm[idx1] - t2*pnm[idx2])/t3           
     return(pnm)
 
-# Calculate arrays of the Associated Legendre Polynomials pnm and the related 
-# values xnm, ynm and znm which are needed to compute the X, Y and Z
-# geomagnetic field components
-#
-# Reverse sign on znm: 16 April 2019
-#
+
 def pxyznm_calc(nmax, th):
-# Initialise
+    """Calculate arrays of the Associated Legendre Polynomials pnm and the related 
+    values xnm, ynm and znm which are needed to compute the X, Y and Z
+    geomagnetic field components
+    Reverse sign on znm: 16 April 2019
+    """
+    # Initialise
     nel   = nmax*(nmax+3)//2+1
     pnm   = np.zeros(nel); xnm = np.zeros(nel)
     ynm   = np.zeros(nel); znm = np.zeros(nel)
@@ -141,7 +155,7 @@ def pxyznm_calc(nmax, th):
     ynm[2] = 1
     znm[0] = 1; znm[1] = 2*ct; znm[2] = 2*st #Check znm[0]
     eps    = 10**(-6)
-#
+
     for i in range(2,nmax+1): # Loop over degree
         idx0 = pnmindex(i,i)
         idx1 = pnmindex(i-1,i-1)
@@ -153,7 +167,7 @@ def pxyznm_calc(nmax, th):
             ynm[idx0] = i*pnm[idx0]/st
         else:
                 ynm[idx0] = xnm[idx0]*ct
-#        
+
         for j in range(i):   # Loop over order
             idx0 = pnmindex(i,j)
             idx1 = pnmindex(i-1,j)
@@ -168,40 +182,41 @@ def pxyznm_calc(nmax, th):
                 ynm[idx0] = j*pnm[idx0]/st
             else:
                 ynm[idx0] = xnm[idx0]*ct
-#                
+
     return((pnm, xnm, ynm, -znm))
 
-# Function to compute values of the geomagnetic field from a model gh of 
-# maximum degree and order nmax for either geodetic ot geocentric coordinates.
-#
+
 def shm_calculator(gh, nmax, altitude, colat, long, coord):
+    """Compute values of the geomagnetic field from a model gh of 
+    maximum degree and order nmax for either geodetic ot geocentric coordinates.
+    """
     RREF     = 6371.2
     degree   = nmax
     phi      = long
-#
+
     if (coord == 'Geodetic'):
         # Geodetic to geocentric conversion
         rad, theta, sd, cd = gd2gc(altitude, colat)
     else:
         rad   = altitude
         theta = colat
-#
-# Create array with  values of (a/r)^(n+2) for n=0,1, 2 ..., degree
+
+    # Create array with  values of (a/r)^(n+2) for n=0,1, 2 ..., degree
     rpow = rad_powers(degree, RREF, rad)
-# Create arrays with cos(m*phi), sin(m*phi)
+    # Create arrays with cos(m*phi), sin(m*phi)
     cmphi, smphi = csmphi(degree,phi)
-# Create arrays with terms such as g(3,2)*cos(2*phi)*(a/r)**5 
+    # Create arrays with terms such as g(3,2)*cos(2*phi)*(a/r)**5 
     ghxz, ghy = gh_phi_rad(gh, degree, cmphi, smphi, rpow)
-# Calculate arrays of the Associated Legendre Polynomials
+    # Calculate arrays of the Associated Legendre Polynomials
     pnm, xnm, ynm, znm = pxyznm_calc(degree, theta)
-# Calculate geomagnetic field components are calculated as a dot product
+    # Calculate geomagnetic field components are calculated as a dot product
     X =  np.dot(ghxz, xnm)
     Y =  np.dot(ghy,  ynm)
     Z =  np.dot(ghxz, znm)
-# Convert back to geodetic (X, Y, Z) if required
+    # Convert back to geodetic (X, Y, Z) if required
     if (coord == 'Geodetic'):
         t = X
         X = X*cd + Z*sd
         Z = Z*cd - t*sd
-#
+
     return((X, Y, Z))
